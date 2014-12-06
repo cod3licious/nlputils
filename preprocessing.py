22a5eed79bf742461d01ce2f778346fb379bb7a3
franzi <cod3licious@gmail.com>
2014-12-06 20:36:18 +0100

    restructuring part 1

---
+++ preprocessing.py
@@ -0,0 +1,142 @@
+from __future__ import division
+import re
+from math import log
+from collections import Counter
+from unidecode import unidecode
+from misc_utils.dict_utils import norm_dict, invert_dict2
+
+def preprocess_text(text):
+    # clean the text: no fucked up characters, html, ...
+    text = unidecode(.decode("utf-8"))
+    text = re.sub(r"http(s)?://\S*", " ", text) # remove links (other html crap is assumed to be removed by bs)
+    text = text.lower()
+    # clean out non-alphabet characters and normalize whitespace
+    text = re.sub(r"[^a-z0-9-]+"," ", text)
+    text = re.sub(r"\s+"," ", text)
+    return text
+
+def get_bigram_scores(text_words, min_bgfreq=2.):
+    """
+    given a dict of document word lists ({did:[w1,w2...,wn]}), compute unigram and bigram frequencies (of words) and give
+    a score for every bigram. depending on this score (e.g. if higher than a threshold), bigram phrases can be identified
+    in the raw texts before splitting them into words
+    min_bgfreq says how often a bigram has to have to occur in the corpus to be recognized (if both words occur just once
+    and that in combination, it still doesn't mean it's a true phrase because we have too little observations of the words
+    --> once the lists of words in the text_words are replaced by appropriate bigrams, the whole thing could be repeated
+        to find trigrams, etc.
+    """
+    unigram_freq = {}
+    bigram_freq = {}
+    for did in text_words:
+        for i, word in enumerate(text_words[did]):
+            # count unigrams and bigrams
+            try:
+                unigram_freq[word] += 1.
+            except:
+                unigram_freq[word] = 1.
+            if i:
+                try:
+                    bigram_freq["%s %s"%(text_words[did][i-1],word)] += 1.
+                except:
+                    bigram_freq["%s %s"%(text_words[did][i-1],word)] = 1.
+    # compute bigram scores
+    bigram_scores = {}
+    for bigram in bigram_freq:
+        # discount to ensure a word combination occurred a sufficient amount of times
+        if max(0.,bigram_freq[bigram]-min_bgfreq):
+            bigram_scores[bigram] = bigram_freq[bigram]/min(unigram_freq[bigram.split()[0]],unigram_freq[bigram.split()[1]])
+    return bigram_scores
+
+def find_bigrams(textdict, threshold=0.75):
+    """
+    find bigrams in the texts
+    Input:
+        - textdict: a dict with {docid: preprocessed_text}
+        - threshold: for bigrams scores
+    Returns:
+        - bigrams: a list of "word1 word2" bigrams
+    """
+    docids = set(textdict.keys())
+    # to identify bigrams, transform the texts into lists of words (assume texts are preprocessed)
+    text_words = {did: textdict[did].split() for did in docids}
+    bigram_scores = get_bigram_scores(text_words)
+    return [bigram for bigram in bigram_scores if bigram_scores[bigram] > threshold]
+
+def replace_bigrams(textdict, bigrams):
+    """
+    replace bigrams in the texts
+    Input:
+        - textdict: a dict with {docid: preprocessed_text}
+        - bigrams: for bigrams scores
+    Returns:
+        - textdict: the same texts but preprocessed and with all bigrams joined as "word1_word2"
+    """
+    docids = set(textdict.keys())
+    # substitute the bigrams in the texts - actually creates trigrams as well :)
+    for did in docids:
+        text = textdict[did]
+        for bigram in bigrams:
+            if bigram in text:
+                text = text.replace(bigram, "%s_%s"%(bigram.split()[0],bigram.split()[1]))
+        textdict[did] = text
+    return textdict
+
+def compute_idf(docfeats):
+    """
+    Inputs:
+        - docfeats: a dict with doc_id:{term:count}
+    Returns:
+        - Dw: a dict with {term: weight}
+    """
+    # total number of documents
+    N = float(len(docfeats))
+    # invert the dictionary to be term:{doc_id:count}
+    termdocs = invert_dict2(docfeats)
+    # compute idf for every term
+    return norm_dict({term:log(N/len(termdocs[term])) for term in termdocs})
+
+def texts2features(textdict, identify_bigrams=True, norm='max', weight=True, renorm='max'):
+    """
+    preprocess texts, count how often each word occurs, weight counts, normalize
+    Input:
+        - textdict: a dict with {docid: text}
+        - identify_bigrams: if bigrams should be found and replaced 
+                            (if it is a list, these are assumed to be the pre-identified bigrams)
+        - norm (binary, max, length, sum, None): how the term counts for each doc should be normalized
+        - weight: if idf term weights should be applied
+                  can also be a dict for precomputed weights, then they will be directly applied 
+        - renorm: how the features with applied weights should be renormalized
+    Returns:
+        - tokendict: a dict with {docid: {}}
+    """
+    docids = set(textdict.keys())
+    # pre-process texts
+    textdict_pp = {preprocess_text(textdict[did]) for did in docids}
+    # possibly find bigrams
+    if identify_bigrams:
+        if isinstance(identify_bigrams, list):
+            bigrams = identify_bigrams
+        else:
+            bigrams = find_bigrams(textdict_pp)
+        textdict_pp = replace_bigrams(textdict_pp, bigrams)
+    # split texts into tokens
+    docfeats = {}
+    for did in docids
+        featdict = dict(Counter(textdict_pp[did].split()))
+        # normalize
+        if norm:
+            featdict = norm_dict(featdict, norm=norm)
+        docfeats[did] = featdict
+    # possibly compute idf weights and re-normalize
+    if weight:
+        if isinstance(weight, dict):
+            Dw = weight
+        else:
+            Dw = compute_idf(docfeats)
+        for did in docids:
+            # if the word was not in Dw (= not in the training set), delete it (otherwise it can mess with renormalization)
+            docfeats[did] = {term:docfeats[did][term]*Dw[term] for term in docfeats[did] if term in Dw}
+            if renorm:
+                docfeats[did] = norm_dict(docfeats[did], norm=renorm)
+    return docfeats
+        
---
+++ trending_words.py
@@ -0,0 +1,171 @@
+from __future__ import division
+import sys
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from misc_utils.dict_utils import invert_dict0, invert_dict1
+
+
+def trending_fun_tpr(tpr, fpr):
+    # to get the development of word occurrences
+    return tpr
+
+def trending_fun_diff(tpr, fpr):
+    # computes the trending score as the difference between tpr and fpr rate (not below 0 though)
+    return np.maximum(tpr-fpr,0.)
+
+def trending_fun_tprmean(tpr, fpr):
+    # computes the trending score as the difference between tpr and fpr rate (not below 0 though)
+    return 0.5*(tpr + np.maximum(tpr-fpr,0.))
+
+def trending_fun_tprmult(tpr, fpr):
+    # computes the trending score as the difference between tpr and fpr rate (not below 0 though)
+    return tpr*np.maximum(tpr-fpr,0.)
+
+def trending_fun_quot(tpr, fpr):
+    # computes the trending score as the difference between tpr and fpr rate (not below 0 though)
+    #return 1./(1.+np.exp(-tpr/np.maximum(fpr,sys.float_info.epsilon)))
+    return (np.minimum(np.maximum(tpr/np.maximum(fpr,sys.float_info.epsilon),1.),4.)-1)/3.
+
+def trending_fun_quotdiff(tpr, fpr):
+    # computes the trending score as the difference between tpr and fpr rate (not below 0 though)
+    #return 1./(1.+np.exp(-tpr/np.maximum(fpr,sys.float_info.epsilon)))
+    return 0.5*(trending_fun_quot(tpr, fpr)+trending_fun_diff(tpr, fpr))
+
+
+def get_k_trending_words(textdict, doccats, k=10, trending_fun=trending_fun_quotdiff, target_cats=[]):
+    """
+    For every category, using the texts belonging to this category vs. max of all other texts, 
+    k 'distinguishing' words are found.
+    Input:
+        - textdict: a dict with {docid:text} (make a deepcopy if you like your original texts)
+        - doccats: a dict with {docid:[cat(s)]} (to get trends in time, cat could also be a year)
+        - k: how many words should be found for every category 
+             --> if k is a list of words, only these words will be kept (if they occur)
+        - trending_fun: which formula should be used when computing the score
+        - target_cats: if not empty, only for some of the documents' categories trending words will be computed
+    Returns:
+        - trending_words: a dict with {cat:[(word1, score1), ..., (wordk,scorek)]}, 
+          i.e. for every category k distinguishing words and a score indicating 
+          how relevant the word is for this category (the higher the better)
+    """
+    # transform texts into sets of words
+    text_words = {did: set(re.findall(r'[a-z_]+', textdict[did])) for did in textdict}
+    # invert this dict to get for every word the documents it occurs in
+    word_dids = {word:set(dids) for word, dids in invert_dict1(text_words).iteritems()}
+    # invert the doccats dict to get for every category a list of documents belonging to it
+    cats_dids = {cat:set(dids) for cat, dids in invert_dict1(doccats).iteritems()}
+    # count the true positives for every word and category
+    tpc_words = {}
+    for word in word_dids:
+        tpc_words[word] = {}
+        for cat in cats_dids:
+            tpc_words[word][cat] = len(cats_dids[cat].intersection(word_dids[word]))/len(cats_dids[cat])
+    # possibly we are only interested in a subset of all categories
+    if not target_cats:
+        target_cats = cats_dids.keys()
+    # for every category, compute a score for every word
+    trending_words = {}
+    for cat in target_cats:
+        cat_wordscores = {}
+        # compute a score for every word
+        for word in word_dids:
+            # in how many of the target category documents the word occurs
+            tpr = tpc_words[word][cat]
+            if tpr:
+                # in how many of the non-target category documents the word occurs (mean+std)
+                ## if the categories have drastically different sizes, maybe use
+                # docids_cat = set(doccats.keys())
+                # otherdocs = docids_cat.difference(cats_dids[cat])
+                # fpr = len(otherdocs.intersection(word_dids[word]))/len(otherdocs)
+                fprs = [tpc_words[word][c] for c in cats_dids if not c == cat]
+                fpr = np.mean(fprs) + np.std(fprs)
+                # compute score
+                cat_wordscores[word] = trending_fun(tpr, fpr)
+        # get the (at most) k relevant words for this category
+        if type(k) == int:
+            trending_words[cat] = [(word,cat_wordscores[word]) for word in sorted(cat_wordscores.keys(), key=cat_wordscores.get, reverse=True)[:min(k,len(cat_wordscores))]]
+        else:
+            trending_words[cat] = [(word,cat_wordscores[word]) if word in cat_wordscores else (word, 0.) for word in k]
+    return trending_words
+
+def get_trending_topics(textdict, trending_words_list, threshold=0.2):
+    """
+    Inputs:
+        - textdict: a dict with {did: text} - most likely you want this to contain only the docs of the target cat
+        - trending_words_list: a list with [(word: score)] like the trending words returned for a single category
+    Returns:
+        - trending_topics: a dict with {topic_id: [trending words]}, i.e. all trending words grouped to topics
+        - topic_docs: a dict with {topic_id: did}, i.e. for every trending topic the best matching document
+    """
+    # get for every word the documents it occurs in
+    text_words = {did: set(re.findall(r'[a-z_]+', textdict[did])) for did in textdict}
+    word_dids = {word:set(dids) for word, dids in invert_dict1(text_words).iteritems()}
+    # for simplicity
+    words =[w[0] for w in trending_words_list]
+    word_scores = {w[0]:w[1] for w in trending_words_list}
+    # compute the similarity between all trending words
+    similarities = {}
+    for i, w1 in enumerate(words[:-1]):
+        for w2 in words[i+1:]:
+            # sim = # docs with w1 and w2 / min # docs with w1 or w2
+            similarities[(w1,w2)] = len(word_dids[w1].intersection(word_dids[w2]))/min(len(word_dids[w1]),len(word_dids[w2]))
+    # assign a topic to every word
+    trending_topics = {i:[w] for i, w in enumerate(words)}
+    # merge topics by median similarity
+    while len(trending_topics) > 1:
+        tpcs = sorted(trending_topics.keys())
+        tpc_sims = {}
+        for i, t1 in enumerate(tpcs[:-1]):
+            for t2 in tpcs[i+1:]:
+                # sim = median similarity of words in topics
+                tpc_sims[(t1,t2)] = np.median([similarities[(w1,w2)] if (w1,w2) in similarities else similarities[(w2,w1)]
+                                               for w1 in trending_topics[t1] for w2 in trending_topics[t2]])
+        # merge the 2 most similar topics - if above threshold
+        (t1,t2) = max(tpc_sims.keys(), key=tpc_sims.get)
+        if tpc_sims[(t1,t2)] < threshold:
+            break
+        trending_topics[t1].extend(trending_topics[t2])
+        del trending_topics[t2]
+    # find the best doc for every topic
+    topic_docs = {}
+    for topic in trending_topics:
+        doc_scores_temp = {}
+        for w in trending_topics[topic]:
+            for did in word_dids[w]:
+                # weight by (normalized) # of occurrences of word?
+                try:
+                    doc_scores_temp[did] += word_scores[w]
+                except:
+                    doc_scores_temp[did] = word_scores[w]
+        # take document with max score
+        topic_docs[topic] = max(doc_scores_temp.keys(), key=doc_scores_temp.get)
+    return trending_topics, topic_docs
+
+
+def test_trending_computations(trending_fun=trending_fun_diff, fun_name='Rate difference'):
+    """
+    given a function to compute the "trending score" of a word given its true and false positive rate,
+    plot the distribution of scores (2D) corresponding to the different tpr and fpr
+    """
+    # make a grid of possible tpr and fpr combinations
+    x, y = np.linspace(0,1,101), np.linspace(1,0,101)
+    fpr, tpr = np.meshgrid(x, y)
+    score = trending_fun(tpr,fpr)
+    plt.figure()
+    plt.imshow(score)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.xticks(np.linspace(0,101,11),np.linspace(0,1,11))
+    plt.yticks(np.linspace(0,101,11),np.linspace(1,0,11))
+    plt.title('Trending Score using %s'%fun_name)
+    plt.colorbar()
+
+
+if __name__ == '__main__':
+    test_trending_computations()
+    test_trending_computations(trending_fun_tprmean, 'tpr mean')
+    test_trending_computations(trending_fun_tprmult, 'tpr mult')
+    test_trending_computations(trending_fun_quot, 'Rate quotient')
+    test_trending_computations(trending_fun_quotdiff, 'Mean of Rate quotient and difference')
+    plt.show()
